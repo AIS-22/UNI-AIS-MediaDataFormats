a0209f930269e437d643b5ad7e2ceb040e839e1b
Moritz Langer <85168282+m-langer@users.noreply.github.com>
2023-04-17 19:20:26 +0200

    add folder struct & crop Img

--- scripts/cropImages.py
+++ scripts/cropImages.py
@@ -4,17 +4,23 @@ import os
 WIDTH = 512
 HEIGHT = 512
 
-path = "/Users/moritzlanger/DEV/FH/AIS-M2/UNI-AIS-MediaDataFormats/Images/DIV2K_train_HR/"
-pathCropped = "/Users/moritzlanger/DEV/FH/AIS-M2/UNI-AIS-MediaDataFormats/Images/Resized/DIV2K_train_HR/"
+path = "../Images/DIV2K_train_HR/"
+pathCropped = "../Images/DIV2K_train_HR/Resized/"
+pathVal = "../Images/DIV2K_valid_HR/"
+pathCroppedVal = "../Images/DIV2K_valid_HR/Resized/"
 
 def crop():
     pngs = []
     for file in os.listdir(path=path):
         if file.endswith(".png"):
             pngs.append(file)
+    pngsVal = []
+    for file in os.listdir(path=pathVal):
+        if file.endswith(".png"):
+            pngsVal.append(file)
     for item in pngs:
-        if os.path.isfile(path+item):
-            im = Image.open(path+item)
+        if os.path.isfile(path + item):
+            im = Image.open(path + item)
             # crop the center of the image
             width, height = im.size
             left = (width - WIDTH)/2
@@ -23,5 +29,17 @@ def crop():
             bottom = (height + HEIGHT)/2
             im = im.crop((left, top, right, bottom))
             im.save(pathCropped + item[:-4] + '.png', 'PNG', quality=100)
+    for item in pngsVal:
+        if os.path.isfile(pathVal + item):
+            im = Image.open(pathVal + item)
+            # crop the center of the image
+            width, height = im.size
+            left = (width - WIDTH)/2
+            top = (height - HEIGHT)/2
+            right = (width + WIDTH)/2
+            bottom = (height + HEIGHT)/2
+            im = im.crop((left, top, right, bottom))
+            im.save(pathCroppedVal + item[:-4] + '.png', 'PNG', quality=100)
 
-crop()
+if __name__ == '__main__':
+    crop()
