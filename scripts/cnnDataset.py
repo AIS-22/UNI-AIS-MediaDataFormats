6a32655f167bbdd68ac5eecdb0e67bf36a247fea
Michael Hafner <fhs44499@fh-salzburg.ac.at>
2023-05-01 16:58:01 +0200

    Add script for classification by pretrained models

--- scripts/cnnDataset.py
+++ scripts/cnnDataset.py
@@ -3,12 +3,12 @@ import torchvision
 from torchvision import transforms
 
 # create a training and test dataset
-def create_dataset():
+def create_dataset(transform=transforms.ToTensor()):
     # create a training dataset from multiple image folders 'Images/DIV2K_train_HR'
-    trainset = torchvision.datasets.ImageFolder(root='Images/DIV2K_train_HR/Decoded/', transform=transforms.ToTensor())
+    trainset = torchvision.datasets.ImageFolder(root='Images/DIV2K_train_HR/Decoded/', transform=transform)
     # target_transform = Lambda(lambda y: torch.zeros(10, dtype=torch.float).scatter_(dim=0, index=torch.tensor(y), value=1))
     # create a test dataset from multiple image folders 'Images/DIV2K_valid_HR'
-    testset = torchvision.datasets.ImageFolder(root='Images/DIV2K_valid_HR/Decoded/', transform=transforms.ToTensor())
+    testset = torchvision.datasets.ImageFolder(root='Images/DIV2K_valid_HR/Decoded/', transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=16, shuffle=True)
     testloader = torch.utils.data.DataLoader(testset, batch_size=16, shuffle=True)
     return trainloader, testloader
