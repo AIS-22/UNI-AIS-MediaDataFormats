6e79677ed4babf9f83b537fdd71961cdd8834627
Aleksandar Radovic <radovic.aleksandar@gmx.at>
2023-05-22 17:29:08 +0200

    add parameter to select folder with one specific filesize

--- scripts/cnnDataset.py
+++ scripts/cnnDataset.py
@@ -3,12 +3,12 @@ import torchvision
 from torchvision import transforms
 
 # create a training and test dataset
-def create_dataset(transform=transforms.ToTensor()):
+def create_dataset(transform=transforms.ToTensor(), filesize=""):
     # create a training dataset from multiple image folders 'Images/DIV2K_train_HR'
-    trainset = torchvision.datasets.ImageFolder(root='Images/DIV2K_train_HR/Decoded/', transform=transform)
+    trainset = torchvision.datasets.ImageFolder(root='Images/DIV2K_train_HR/Decoded'+filesize+'/', transform=transform)
     # target_transform = Lambda(lambda y: torch.zeros(10, dtype=torch.float).scatter_(dim=0, index=torch.tensor(y), value=1))
     # create a test dataset from multiple image folders 'Images/DIV2K_valid_HR'
-    testset = torchvision.datasets.ImageFolder(root='Images/DIV2K_valid_HR/Decoded/', transform=transform)
+    testset = torchvision.datasets.ImageFolder(root='Images/DIV2K_valid_HR/Decoded'+filesize+'/', transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=16, shuffle=True)
     testloader = torch.utils.data.DataLoader(testset, batch_size=16, shuffle=True)
     return trainloader, testloader
