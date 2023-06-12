afc2791b6ae798c151bda7eaa4665d4a6b586ab3
Michael Hafner <fhs44499@fh-salzburg.ac.at>
2023-06-12 13:23:20 +0200

    Adapt code for specific filesize processing

--- scripts/pretrainedCNNFilesize.py
+++ scripts/pretrainedCNNFilesize.py
@@ -1,9 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import numpy as np
 from torchvision import transforms, models
 from sklearn.metrics import precision_recall_fscore_support
 import cnnDataset
+import evaluateFilesizeModel
 
 
 def evaluate_model(model, test_loader):
@@ -34,8 +36,6 @@ def evaluate_model(model, test_loader):
     print(f'F1-score: {f1_score:.4f}')
 
 
-
-
 device = (
     "cuda"
     if torch.cuda.is_available()
@@ -44,35 +44,29 @@ device = (
     else "cpu"
 )
 
-filesizes = ['40', '50', '60', '75', '100']
+filesizes = ['5', '10', '17', '25', '32', '40', '50', '60', '75', '100']
 
 for filesize in filesizes:
     transform = transforms.Compose([
-    # the model excepts just 224x224 images, maybe we need to crop our images before encoding them
-    # I used crop, resize would change the image and therefore can lead to probles for our classification
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        # the model excepts just 224x224 images, maybe we need to crop our images before encoding them
+        # I used crop, resize would change the image and therefore can lead to probles for our classification
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
     print('-------------------------------------------------------------------------------------')
     print('Train model with filesize = ' + filesize + ' kB')
     train_loader, val_loader = cnnDataset.create_dataset(transform=transform, filesize=filesize)
 
-    model_name = 'vgg16'
-    model = models.vgg16(weights=models.VGG16_Weights.DEFAULT)
+    # resNet
+    model_name = 'resnet18'
+    model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+
     num_new_classes = 6
 
-    """
-    VGG16 Network (model):
-    Index 0: Fully connected layer (nn.Linear)
-    Index 1: ReLU activation (nn.ReLU)
-    Index 2: Dropout layer (nn.Dropout)
-    Index 3: Fully connected layer (nn.Linear)
-    Index 4: ReLU activation (nn.ReLU)
-    Index 5: Dropout layer (nn.Dropout)
-    Index 6: Fully connected layer (nn.Linear) -- The output layer which needs to be replaced
-    """
-    model.classifier[6] = nn.Linear(model.classifier[6].in_features, num_new_classes)
+    # use this if e.g. resNet get used
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, num_new_classes)  # Replace the final layer with the number of codec classes
     model = model.to(device)
 
     criterion = nn.CrossEntropyLoss()
@@ -81,8 +75,10 @@ for filesize in filesizes:
 
     # change the number of epochs here!
     num_epochs = 10
+    losses = np.zeros((num_epochs, 2))
 
     for epoch in range(num_epochs):
+        print(f'Start to train epoch: {epoch + 1}')
         model.train()
         running_loss = 0.0
         for images, labels in train_loader:
@@ -102,6 +98,7 @@ for filesize in filesizes:
         model.eval()
         correct = 0
         total = 0
+        test_loss = 0
         with torch.no_grad():
             for images, labels in val_loader:
                 images, labels = images.to(device), labels.to(device)
@@ -110,13 +107,21 @@ for filesize in filesizes:
                 _, predicted = torch.max(outputs.data, 1)
                 total += labels.size(0)  # Update the total count of processed samples
                 correct += (predicted == labels).sum().item()
+                test_loss += criterion(outputs, labels).item() * images.size(0)
 
         accuracy = correct / total
-        print(f'Validation accuracy: {accuracy:.4f} in epoch: {epoch + 1}')
+        test_loss /= len(val_loader.dataset.samples)
+        print(f'Validation accuracy: {accuracy:.4f} loss: {test_loss} in epoch: {epoch + 1}')
+        losses[epoch, 0] = epoch_loss
+        losses[epoch, 1] = test_loss
+    # store the results in a file
+    np.save('results/losses_' + filesize + '_model.npy', losses)
 
     evaluate_model(model, val_loader)
 
-    save_model = True 
+    save_model = True
     if save_model:
-        torch.save(model, "models/cnnParams_" + model_name +"_Filesize_" + filesize + ".pt")
+        torch.save(model, "models/cnnParams_" + model_name + "_fs_" + filesize + ".pt")
         print("Model saved")
+
+evaluateFilesizeModel.main()
