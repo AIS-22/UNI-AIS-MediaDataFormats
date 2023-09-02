e05947eda9582e9e8c47fa42d8ce0c43f4c3c0c5
Aleksandar Radovic <radovic.aleksandar@gmx.at>
2023-09-02 11:15:50 +0200

    add check for images in filesize folder

--- scripts/checkCounts.sh
+++ scripts/checkCounts.sh
@@ -3,13 +3,20 @@ codecs=( AVIF BPG HEIC JPEG JPEG2000 JPEG_XL JPEG_XR_0 JPEG_1 JPEG_XR_1 JPEG_XR_
 echo "train"
 echo "param"
 cd Images/DIV2K_train_HR
+# find . -maxdepth 1 -mindepth 1 -type d | while read dir; do
+#   printf "%-25.25s : " "$dir"
+#   find "$dir" -type f | wc -l
+# done
+
+echo "decoded"
+cd Decoded_pieces
 find . -maxdepth 1 -mindepth 1 -type d | while read dir; do
   printf "%-25.25s : " "$dir"
   find "$dir" -type f | wc -l
 done
 
-echo "decoded"
-cd Decoded
+echo $1
+cd $1
 find . -maxdepth 1 -mindepth 1 -type d | while read dir; do
   printf "%-25.25s : " "$dir"
   find "$dir" -type f | wc -l
@@ -17,15 +24,22 @@ done
 
 echo "valid"
 echo "param"
-cd ../../..
+cd ../../../..
 cd Images/DIV2K_valid_HR
+# find . -maxdepth 1 -mindepth 1 -type d | while read dir; do
+#   printf "%-25.25s : " "$dir"
+#   find "$dir" -type f | wc -l
+# done
+
+echo "decoded"
+cd Decoded_pieces
 find . -maxdepth 1 -mindepth 1 -type d | while read dir; do
   printf "%-25.25s : " "$dir"
   find "$dir" -type f | wc -l
 done
 
-echo "decoded"
-cd Decoded
+echo $1
+cd $1
 find . -maxdepth 1 -mindepth 1 -type d | while read dir; do
   printf "%-25.25s : " "$dir"
   find "$dir" -type f | wc -l
