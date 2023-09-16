90737f5bea35ba0f0a21919b4a458e739223baf4
Aleksandar Radovic <radovic.aleksandar@gmx.at>
2023-09-16 12:24:17 +0200

    add function to plot big conf matrix

--- scripts/plotResults.py
+++ scripts/plotResults.py
@@ -16,17 +16,17 @@ def plot_accuracy_results():
     cmap = plt.get_cmap('tab20')
     plt.figure(figsize=(20, 10))
     plt.plot(keys, mixed_results.values(), color=cmap(0), label='Mixed pre trained (5-32)')
+     
+    mixed_self_results = np.load(RESULTS_FOLDER + 'accuracy_mixed_self_model.npy', allow_pickle=True).item()
+    keys = [int(key) for key in mixed_self_results.keys()]
+    plt.figure(figsize=(20, 10))
+    plt.plot(keys, mixed_self_results.values(), color=cmap(0), label='Mixed self trained (5-32)')
 
     for i, filesize in enumerate(filesizes):
         res_dict = np.load(RESULTS_FOLDER + 'accuracy_fs_' + filesize + '_model.npy', allow_pickle=True).item()
         keys = [int(key) for key in res_dict.keys()]
         plt.plot(keys, res_dict.values(), color=cmap(i + 1),
                  label=filesize + f'  c-rate: {(AVG_FILESIZE / float(filesize)):.2f}')
-        
-    mixed_self_results = np.load(RESULTS_FOLDER + 'accuracy_mixed_self_model.npy', allow_pickle=True).item()
-    keys = [int(key) for key in mixed_self_results.keys()]
-    plt.figure(figsize=(20, 10))
-    plt.plot(keys, mixed_self_results.values(), color=cmap(0), label='Mixed self trained (5-32)')
 
     plt.grid()   
     plt.xlabel('Test Filesize')
@@ -94,7 +94,61 @@ def plot_confusion_matrix():
             plt.xticks(rotation=45)
             plt.tight_layout()
             plt.savefig('Plots/conf_matrix/' + fs + '/conf_matrix_fs_'+ ev_size + '.png')
-   
+
+def plot_confusion_matrix_all():
+    filesizes = ['5', '10', '17', '25', '32', '40', '50', '60', '75', '100']
+    categories = ['AVIF', 'BPG', 'HEIC', 'JPEG', 'JPEG2000', 'JPEG_XL', 'JPEG_XR_0', 'JPEG_XR_1', 'JPEG_XR_2', 'WEBP']
+    # mixed model
+    # load dic from file
+    mixed_results = np.load(RESULTS_FOLDER + 'conf_matrix_all_mixed_model.npy', allow_pickle=True).tolist()
+    cmap = plt.get_cmap('tab20')
+    
+    plt.figure(figsize=(15,10))
+    sn.set(font_scale=1.4)
+    sn.heatmap(mixed_results, vmin=0, vmax=1000,
+            xticklabels=categories, 
+            yticklabels=categories, 
+            annot=True, 
+            cmap='Blues',
+            fmt=".0f",
+            annot_kws={'fontsize': 20})
+    plt.xticks(rotation=45)
+    plt.savefig('Plots/conf_matrix/mixed/conf_matrix_all_mixed_model.png')
+
+    # mixed self model
+    # load dic from file
+    mixed_self_results = np.load(RESULTS_FOLDER + 'conf_matrix_all_mixed_self_model.npy', allow_pickle=True).tolist()
+    
+    plt.figure(figsize=(15,10))
+    sn.set(font_scale=1.4)
+    sn.heatmap(mixed_self_results, vmin=0, vmax=1000,
+            xticklabels=categories, 
+            yticklabels=categories, 
+            annot=True, 
+            cmap='Blues',
+            fmt=".0f",
+            annot_kws={'fontsize': 20})
+    plt.xticks(rotation=45)
+    plt.savefig('Plots/conf_matrix/mixed_self/conf_matrix_all_mixed_self_model.png')
+
+    # filesize models    
+    for fs in filesizes:
+        # load dic from file
+        fs_results = np.load(RESULTS_FOLDER + 'conf_matrix_all_fs_' + fs + '_model.npy', allow_pickle=True).tolist()
+        plt.figure(figsize=(15,10))
+        sn.set(font_scale=1.4)
+        sn.heatmap(fs_results, vmin=0, vmax=50000,
+               xticklabels=categories, 
+               yticklabels=categories, 
+               annot=True, 
+               cmap='Blues',
+                fmt=".0f",
+               annot_kws={'fontsize': 20})
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plt.savefig('Plots/conf_matrix/'+ fs + '/conf_matrix_all_fs_'+ fs + '.png')
+
+
 def plot_loss_results():
     filesizes = ['5', '10', '17', '25', '32', '40', '50', '60', '75', '100']
 
@@ -125,9 +179,6 @@ def plot_loss_results():
         plt.legend(title='Loss Functions')
         plt.savefig('Plots/loss_comparison_fs_' + filesize + '_model.png')
 
-    
-
-
 def plot_dec_enc_time():
     exec_time_dict = {
         'avif': 0.24,
@@ -150,10 +201,11 @@ def plot_dec_enc_time():
 
 
 def main():
-    # plot_accuracy_results()
+    plot_accuracy_results()
+    #plot_confusion_matrix()
+    #plot_confusion_matrix_all()
     # plot_loss_results()
     # plot_dec_enc_time()
-    plot_confusion_matrix()
 
 
 if __name__ == '__main__':
