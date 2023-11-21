dae1190914d03505219d31d6ce3da10729358a48
Stefan Findenig <s.findenig@gmail.com>
2023-11-21 14:39:50 +0100

    Move MultiCrop into encode_heic

--- scripts/heicEnc.py
+++ scripts/heicEnc.py
@@ -11,14 +11,6 @@ maxQ = 100
 trainFolder = 'DIV2K_train_HR/'
 validFolder = 'DIV2K_valid_HR/'
 usedCodec = 'HEIC/'
-
-useMultiCropPerImage = True
-if useMultiCropPerImage:
-    decodedFolder = 'Decoded_pieces/'
-    croppedFolder = 'ResizedInPieces/'
-else:
-    decodedFolder = 'Decoded/'
-    croppedFolder = 'Resized/'
     
 outputPrefix = 'heic_'
 outputFileExtension = '.heic'
@@ -34,8 +26,14 @@ def decode_heic(enc_file, dec_file):
     dec_filesize_folder = dec_file.replace('all', file_size)
     image.save(dec_filesize_folder, quality=100)
 
-def encode_heic(printProgress=False, maxFileSizeKb = 32):
+def encode_heic(printProgress=False, maxFileSizeKb = 32, useMultiCropPerImage = True):
     i = 0
+    if useMultiCropPerImage:
+        decodedFolder = 'Decoded_pieces/'
+        croppedFolder = 'ResizedInPieces/'
+    else:
+        decodedFolder = 'Decoded/'
+        croppedFolder = 'Resized/'
 #    number_of_files = len(glob.glob('Images/' + '*/' + '*' + pngExtension))
     number_of_files = len(glob.glob('Images/*/' + croppedFolder + '/*.png'))
 
