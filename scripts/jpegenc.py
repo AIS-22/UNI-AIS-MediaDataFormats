4fcf4367771adfb5cf6d07873f1fc13d232f70be
Stefan Findenig <s.findenig@gmail.com>
2023-11-21 14:41:38 +0100

    Move MultiCropt into encode_jpeg

--- scripts/jpegenc.py
+++ scripts/jpegenc.py
@@ -11,14 +11,6 @@ validFolder = 'DIV2K_valid_HR/'
 availableSubFolder = [trainFolder, validFolder]
 usedCodec = 'JPEG/'
 
-useMultiCropPerImage = True
-if useMultiCropPerImage:
-    decodedFolder = 'Decoded_pieces/'
-    croppedFolder = 'ResizedInPieces/'
-else:
-    decodedFolder = 'Decoded/'
-    croppedFolder = 'Resized/'
-
 outputPrefix = 'jpeg_'
 outputFileExtension = '.jpg'
 pngExtension = '.png'
@@ -31,8 +23,14 @@ def decode_jpeg(enc_file, dec_file):
     dec_filesize_folder = dec_file.replace('all', file_size)
     image.save(dec_filesize_folder, quality=100)
 
-def encode_jpeg(printProgress=False, maxFileSizeKb = 32):
+def encode_jpeg(printProgress=False, maxFileSizeKb = 32, useMultiCropPerImage = True):
     i = 0
+    if useMultiCropPerImage:
+        decodedFolder = 'Decoded_pieces/'
+        croppedFolder = 'ResizedInPieces/'
+    else:
+        decodedFolder = 'Decoded/'
+        croppedFolder = 'Resized/'
     #number_of_files = len(glob.glob('Images/' + '*/' + '*' + pngExtension))
     number_of_files = len(glob.glob('Images/*/' + croppedFolder + '/*.png'))
     for subFolder in availableSubFolder:
