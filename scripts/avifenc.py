4179ae5dffca3241774766505a79d0d7cf2e8aad
Stefan Findenig <s.findenig@gmail.com>
2023-11-21 14:37:38 +0100

    Move useMultiCropPerImage into function encode_avif

--- scripts/avifenc.py
+++ scripts/avifenc.py
@@ -14,13 +14,8 @@ validFolder = 'DIV2K_valid_HR/'
 availableSubFolder = [trainFolder, validFolder]
 usedCodec = 'AVIF/'
 
-useMultiCropPerImage = True
-if useMultiCropPerImage:
-    decodedFolder = 'Decoded_pieces/'
-    croppedFolder = 'ResizedInPieces/'
-else:
-    decodedFolder = 'Decoded/'
-    croppedFolder = 'Resized/'
+
+
 
 outputPrefix = 'avif_'
 outputFileExtension = '.avif'
@@ -34,8 +29,14 @@ def decode_avif(enc_file, dec_file):
     dec_filesize_folder = dec_file.replace('all', file_size)
     image.save(dec_filesize_folder, quality=100)
 
-def encode_avif(printProgress=False, maxFileSizeKb = 32):
+def encode_avif(printProgress=False, maxFileSizeKb = 32, useMultiCropPerImage = True):
     i = 0
+    if useMultiCropPerImage:
+        decodedFolder = 'Decoded_pieces/'
+        croppedFolder = 'ResizedInPieces/'
+    else:
+        decodedFolder = 'Decoded/'
+        croppedFolder = 'Resized/'
     #number_of_files = len(glob.glob('Images/' + '*/' + '*' + pngExtension))
     number_of_files = len(glob.glob('Images/*/' + croppedFolder + '*.png'))
     for subFolder in availableSubFolder:
