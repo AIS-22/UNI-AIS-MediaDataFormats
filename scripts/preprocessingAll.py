086cc05973bbf92e532adb7fb2848cf21791fefe
Moritz Langer <moritz.langer@redbull.com>
2024-01-30 14:24:07 +0100

    add model with files sizes 5 - 100

--- scripts/preprocessingAll.py
+++ scripts/preprocessingAll.py
@@ -22,26 +22,26 @@ async def preprocess(cropNeeded=False,
         # first crop images
         print('Start to crop images to {}x{} pixel.'.format(width, height))
         cropImages.crop(width, height)
-        
+
     filesizes = [5, 10, 17, 25, 32, 40, 50, 60, 75, 100]
     for maxFileSizeKb in filesizes:
         # start the encoding and decoding task of each codec asynchronously
         print('Start to encode images. Filesize='+str(maxFileSizeKb))
         with ThreadPoolExecutor() as executor:
             futures = [
-                executor.submit(avifenc.encode_avif, printProgress, maxFileSizeKb),  
-                executor.submit(bpgenc.encode_bpg, printProgress, maxFileSizeKb),     
-                executor.submit(heicEnc.encode_heic, printProgress, maxFileSizeKb),   
-                executor.submit(jpegenc.encode_jpeg, printProgress, maxFileSizeKb),   
-                executor.submit(jpeg2000enc.encode_jpeg2k, printProgress, maxFileSizeKb), 
-                executor.submit(jpegxl.encode_jpgxl, printProgress, maxFileSizeKb),   
-                executor.submit(jxrenc_0.encode_jxr, printProgress, maxFileSizeKb),   
-                executor.submit(jxrenc_1.encode_jxr, printProgress, maxFileSizeKb),   
-                executor.submit(jxrenc_2.encode_jxr, printProgress, maxFileSizeKb),   
+                executor.submit(avifenc.encode_avif, printProgress, maxFileSizeKb),
+                executor.submit(bpgenc.encode_bpg, printProgress, maxFileSizeKb),
+                executor.submit(heicEnc.encode_heic, printProgress, maxFileSizeKb),
+                executor.submit(jpegenc.encode_jpeg, printProgress, maxFileSizeKb),
+                executor.submit(jpeg2000enc.encode_jpeg2k, printProgress, maxFileSizeKb),
+                executor.submit(jpegxl.encode_jpgxl, printProgress, maxFileSizeKb),
+                executor.submit(jxrenc_0.encode_jxr, printProgress, maxFileSizeKb),
                 executor.submit(webP.encode_webp, printProgress, maxFileSizeKb),
             ]
             for future in futures:
                 future.result()
+        jxrenc_1.encode_jxr(printProgress, maxFileSizeKb)
+        jxrenc_2.encode_jxr(printProgress, maxFileSizeKb)
         print('Finished preprocessing job.')
 
 
